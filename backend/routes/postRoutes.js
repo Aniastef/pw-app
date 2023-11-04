10593cd6105eb92b2044b10598dfaf8025960e1f
IstvanStefania <stefania.istvan@student.upt.ro>
2023-11-05 00:25:23 +0200

    am facut un fisier separat pentru frontend si unul pt backend

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
-import {createPost} from "../controllers/postController.js"
+import {createPost, getPost,deletePost,likeUnlikePost,replyToPost,getFeedPosts} from "../controllers/postController.js"
 import protectRoute from "../middlewares/protectRoute.js"
+
 
 const router=express.Router();
 
+router.get("/feed",protectRoute,getFeedPosts)
 router.post("/create",protectRoute,createPost)
+router.get("/:id",getPost)
+router.delete("/:id",protectRoute,deletePost)
+router.post("/like/:id",protectRoute,likeUnlikePost)
+router.post("/reply/:id",protectRoute,replyToPost)
 
 export default router;
